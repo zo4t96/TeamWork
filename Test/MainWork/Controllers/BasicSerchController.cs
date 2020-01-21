a07c0edd9c60e1b8b2eed2067d8029dce90283ec
zo4t96 <zo4t96@gmail.com>
2020-01-21 09:05:53 +0800

    第一次上傳期末專題測試

---
+++ Test/MainWork/Controllers/BasicSerchController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MainWork.Controllers
+{
+    public class BasicSerchController : Controller
+    {
+        // GET: BasicSerch
+        public ActionResult BasicSerch()
+        {
+            dbProjectMusicStoreEntities db = new dbProjectMusicStoreEntities();
+            var at = db.tAlbumTypes.Select(p => p);
+            return View(at);
+        }
+    }
+}
---
+++ Test/MainWork/Controllers/UploadTestController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MainWork.Controllers
+{
+    public class UploadTestController : Controller
+    {
+        // GET: UploadTest
+        public ActionResult upload()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult upload(tUpload u)
+        {
+            //若上傳檔案超出最大限制(4mb)，到Web.config的<system.web>標籤內的<httpRuntime>標籤添加新的屬性↓
+            //maxRequestLength="想更改的長度限制"，單位是KB
+            
+            //string name = Path.GetFileName(u.file.FileName) + ".png";
+            string name = Guid.NewGuid().ToString() + ".mp3";
+            var path = Path.Combine(Server.MapPath("~/Music"), name);
+
+
+            u.file.SaveAs(path);
+            u.fDate = DateTime.Now;
+            u.fPath = "/Music/" + name;
+            dbTestEntities db = new dbTestEntities();
+            db.tUpload.Add(u);
+            db.SaveChanges();
+            return View();
+        }
+    }
+}
