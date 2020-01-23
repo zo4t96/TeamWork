fe33d19c260469dbffb0b43051e217459876d55e
zo4t96 <zo4t96@gmail.com>
2020-01-23 10:20:20 +0800

    新增一些測試用的程式碼.更新母版等等

--- Test/MainWork/App_Start/RouteConfig.cs
+++ Test/MainWork/App_Start/RouteConfig.cs
@@ -14,9 +14,9 @@ namespace MainWork
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
+                name: "MyMusic",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Homepage", action = "homepage", id = UrlParameter.Optional }
             );
         }
     }
