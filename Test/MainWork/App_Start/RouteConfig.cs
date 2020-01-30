672cf2695d97b2a4cfd5083cf31af7a517e00760
zo4t96 <zo4t96@gmail.com>
2020-01-30 08:48:43 +0800

    教室電腦先commit試試看?

--- Test/MainWork/App_Start/RouteConfig.cs
+++ Test/MainWork/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MainWork
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Homepage", action = "homepage", id = UrlParameter.Optional }
             );
         }
     }
---
+++ Test/MainWork/Controllers/HomepageController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MainWork.Controllers
+{
+    public class HomepageController : Controller
+    {
+        // GET: Homepage
+        public ActionResult homepage()
+        {
+            return View();
+        }
+
+        public PartialViewResult BasicSerchPartial()
+        {
+            dbProjectMusicStoreEntities db = new dbProjectMusicStoreEntities();
+            var at = db.tAlbumTypes.Select(p => p);
+            return PartialView(at);
+        }
+    }
+}
